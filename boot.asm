692e42ceb3dd9137ee0c1e3bcce21ad97f03af93
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-04-13 16:50:44 -0300

    Create boot.asm

---
+++ boot.asm
@@ -0,0 +1,21 @@
+section .data
+    last_response db 0
+
+section .text
+    global _start
+
+_start
+    mov ecx, url_repo
+    mov ecx, interval
+
+loop:
+    mov eax, 0
+    cmp al, [last_response]
+    je no_update
+    mov eax, 1
+    mov [last_response], al
+
+last_responde:
+    mov eax, 2
+
+    jmp loop
